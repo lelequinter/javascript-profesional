afc93ff3a177e57a2efe8a3cf3f9d33d213f7406
lelequinter <lele.quinter@gmail.com>
2023-02-02 21:02:57 -0500

    El primer plugin

--- website/assets/MediaPlayer.js
+++ website/assets/MediaPlayer.js
@@ -1,10 +1,39 @@
-
 function MediaPlayer(config) {
   this.video = config.el;
+  this.plugins = config.plugins || [];
+
+  this._initPlugins();
 }
 
-MediaPlayer.prototype.playPause = function () {
-  this.video.paused ? this.video.play() : this.video.pause();
+MediaPlayer.prototype._initPlugins = function () {
+  this.plugins.forEach((plugin) => {
+    plugin.run(this);
+  });
+};
+
+MediaPlayer.prototype.play = function () {
+  this.video.play();
+};
+
+MediaPlayer.prototype.pause = function () {
+  this.video.pause();
+};
+
+MediaPlayer.prototype.togglePlay = function () {
+  this.video.paused ? this.play() : this.pause();
+};
+
+MediaPlayer.prototype.mute = function () {
+  this.video.muted = true;
+};
+
+MediaPlayer.prototype.unmute = function () {
+  this.video.muted = false;
+};
+
+MediaPlayer.prototype.toggleMute = function () {
+  this.video.muted ? (this.video.muted = false) : (this.video.muted = true);
+  // console.log('Muteo:: ', this.video.muted)
 };
 
 export default MediaPlayer;
--- website/assets/index.js
+++ website/assets/index.js
@@ -1,9 +1,15 @@
 import MediaPlayer from "./MediaPlayer.js";
+import Autoplay from "./plugins/AutoPlay.js";
 
 const video = document.querySelector("video");
-const player = new MediaPlayer({ el: video });
+const player = new MediaPlayer({ el: video, plugins: [new Autoplay()] });
 
 const buttonPlay = document.getElementById("playButton");
 buttonPlay.onclick = () => {
-  player.playPause();
+  player.togglePlay();
 };
+
+const buttonMute = document.getElementById('muteButton');
+buttonMute.onclick = () => {
+  player.toggleMute();
+}
