81e5722e0a396f858df29064c9c8a209ffff0a2b
lelequinter <lele.quinter@gmail.com>
2023-02-08 19:51:14 -0500

    this

--- website/assets/index.js
+++ website/assets/index.js
@@ -2,7 +2,9 @@ import MediaPlayer from "./MediaPlayer.js";
 import Autoplay from "./plugins/AutoPlay.js";
 
 const video = document.querySelector("video");
-const player = new MediaPlayer({ el: video, plugins: [new Autoplay()] });
+const player = new MediaPlayer({ el: video, plugins: [
+  // new Autoplay()
+] });
 
 const buttonPlay = document.getElementById("playButton");
 buttonPlay.onclick = () => {
